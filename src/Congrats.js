611f9b78f50075061eba1a02d99c0dd8014094ab
Tamara Tibold <tamara.milosavljevic89@gmail.com>
2019-10-29 22:42:00 +0100

    tested Congrats component

---
+++ src/Congrats.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Congrats = props => {
+  {
+    if (props.success) {
+      return (
+        <div data-test="component-congrats">
+          <span data-test="congrats-message">
+            Congratilations! you guessed the word!
+          </span>
+        </div>
+      );
+    } else {
+      return <div data-test="component-congrats" />;
+    }
+  }
+};
+export default Congrats;
