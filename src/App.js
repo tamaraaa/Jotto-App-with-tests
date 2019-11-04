b0d01b38680f05f6d3c943320ee0b4015a169d19
Tamara Tibold <tamara.milosavljevic89@gmail.com>
2019-11-04 14:58:28 +0100

    add bootstrap styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
-function App() {
+import GuessedWords from "./GuessedWords";
+import Congrats from "./Congrats";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Jotto</h1>
+      <Congrats success={true} />
+      <GuessedWords
+        guessedWords={[{ guessedWord: "train", letterMatchCount: 3 }]}
+      />
     </div>
   );
-}
+};
 
 export default App;
--- src/Congrats.js
+++ src/Congrats.js
@@ -1,19 +1,16 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 const Congrats = props => {
-  {
-    if (props.success) {
-      return (
-        <div data-test="component-congrats">
-          <span data-test="congrats-message">
-            Congratilations! you guessed the word!
-          </span>
-        </div>
-      );
-    } else {
-      return <div data-test="component-congrats" />;
-    }
+  if (props.success) {
+    return (
+      <div data-test="component-congrats" className="alert alert-success">
+        <span data-test="congrats-message">
+          Congratilations! you guessed the word!
+        </span>
+      </div>
+    );
+  } else {
+    return <div data-test="component-congrats" />;
   }
 };
 export default Congrats;
