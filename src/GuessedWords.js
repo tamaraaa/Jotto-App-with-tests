425157b66a05d3fb4ff3a6a20a03f4829ac18407
Tamara Tibold <tamara.milosavljevic89@gmail.com>
2019-11-04 14:07:55 +0100

    test and code for guessed words

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -6,6 +6,29 @@ const GuessedWords = props => {
     contents = (
       <span data-test="guess-instructions">Guess the secret word</span>
     );
+  } else {
+    const guessedWordsRow = props.guessedWords.map((word, index) => {
+      return (
+        <tr data-test="guessed-word" key={index}>
+          <td>{word.guessedWord}</td>
+          <td>{word.letterMatchCount}</td>
+        </tr>
+      );
+    });
+    contents = (
+      <div data-test="guessed-words">
+        <h3>Guessed Words</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Guess</th>
+              <th>Matching Letters</th>
+            </tr>
+          </thead>
+          <tbody>{guessedWordsRow}</tbody>
+        </table>
+      </div>
+    );
   }
   return <div data-test="component-guessed-words">{contents}</div>;
 };
