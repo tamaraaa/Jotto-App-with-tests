365900672dbf80c84241f117defe8e425b403937
Tamara Tibold <tamara.milosavljevic89@gmail.com>
2019-11-05 14:46:36 +0100

    setup redux

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,6 +1,7 @@
 export const getLetterMatchCount = (guessedWord, secretWord) => {
   const secretLetterSet = new Set(secretWord.split(""));
   const guessedLetterSet = new Set(guessedWord.split(""));
+  console.log(typeof guessedLetterSet);
   return [...secretLetterSet].filter(letter => guessedLetterSet.has(letter))
     .length;
 };
